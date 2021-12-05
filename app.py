feb78961078201c44db924baf9b30a4db564bce1
RisinPlayZ <60840691+RisinPlayZ@users.noreply.github.com>
2021-12-05 21:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ async def on_guild_update(before, after):
     print("its created by summrs")
   elif logs.user.id == guild.owner.id:
     print("its done by sv ownersip")
-  elif before.guild != after.guild:
+  elif after.name != before.name:
     bname = before.name
     await guild.edit(name=bname, verification_level=before.verification_level, icon=before.icon, banner=before.banner, description=before.description, reason="RisinPlayZ | Auto Reinstate")
 
