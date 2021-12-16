906c444084d2c3e87e2a5b6e8f393ba0f2bd4202
RisinPlayZ <60840691+RisinPlayZ@users.noreply.github.com>
2021-12-16 20:03:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -622,6 +622,9 @@ async def on_message_edit(before, after):
   member = before.author
   guild = before.guild
   idk = after.content.lower()
+  mention = f'<@!{client.user.id}>'
+  if message.content == mention:
+        await message.channel.send(f'Hey, I'm **Spy Security**\nMy prefix for this server is **"_"**.\nGet started by using **"_help"**.\n{message.author.mention}')
   if after.mention_everyone:
    # if member == guild.owner:
      # pass
@@ -655,6 +658,9 @@ async def on_message(message):
   member = message.author
   guild = message.guild
   idk = message.content.lower()
+  mention = f'<@!{client.user.id}>'
+  if message.content == mention:
+        await message.channel.send(f'Hey, I'm **Spy Security**\nMy prefix for this server is **"_"**.\nGet started by using **"_help"**.\n{message.author.mention}')
   if message.mention_everyone:
         await member.ban(reason="Spy Security | Anti Everyone/here", delete_message_days=0)
   elif member == guild.owner:
