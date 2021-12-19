c00df4a8257ed85e26daae899a563df71c622601
RisinPlayZ <60840691+RisinPlayZ@users.noreply.github.com>
2021-12-19 15:27:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ os.system("pip install aiohttp")
 import aiohttp
 os.system("pip install logging")
 import logging
-from discord.ext import commands
+from discord.ext import commands, MissingPermissions
 os.system("pip install colorama")
 import colorama
 os.system("pip install colored")
